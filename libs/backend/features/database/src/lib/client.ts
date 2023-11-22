66e285d00bf0b29ec3c86cec0cd753418b289d4f
enea-scaccabarozzi <me@eneascaccabarozzi.xyz>
2023-11-22 16:28:53 +0100

    feat: add support fo enviroment variables validations

--- libs/backend/features/database/src/lib/client.ts
+++ libs/backend/features/database/src/lib/client.ts
@@ -1,3 +1,4 @@
+import { env } from '@portfolio/backend-shared';
 import { PrismaClient } from '@prisma/client';
 
 export * from '@prisma/client';
@@ -8,9 +9,7 @@ export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log:
-      process.env.NODE_ENV === 'development'
-        ? ['query', 'error', 'warn']
-        : ['error'],
+      env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
