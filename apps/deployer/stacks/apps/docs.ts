2c5c517f3f505ac0a7da9be192cdded8b172ab8f
enea-scaccabarozzi <me@eneascaccabarozzi.xyz>
2023-12-03 12:35:20 +0100

    feat: add sst support for custom domains

--- apps/deployer/stacks/apps/docs.ts
+++ apps/deployer/stacks/apps/docs.ts
@@ -1,4 +1,5 @@
 import { NextjsSite, StackContext, use } from 'sst/constructs';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 
 import { Configs } from '../config';
 
@@ -6,9 +7,23 @@ export const Docs = ({ app, stack }: StackContext) => {
   const docs = new NextjsSite(stack, 'docs', {
     bind: [...use(Configs).config],
     path: './apps/docs',
+    buildCommand: 'SKIP_ENV_VALIDATION=true sst bind next build',
+    customDomain: {
+      isExternalDomain: true,
+      domainName: `${
+        process.env.SST_STAGE === 'staging' ? 'staging-' : ''
+      }docs.eneascaccabarozzi.xyz`,
+      cdk: {
+        certificate: Certificate.fromCertificateArn(
+          stack,
+          'SslCert',
+          'arn:aws:acm:us-east-1:909221929648:certificate/23567966-5942-4f8f-92a9-e1f277474abc'
+        ),
+      },
+    },
   });
 
   stack.addOutputs({
-    url: docs.url,
+    url: docs.customDomainUrl ?? docs.url,
   });
 };
