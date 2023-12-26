757388cc2d94eeae1cdb3eec04a94eba804a6a62
enea-scaccabarozzi <me@eneascaccabarozzi.xyz>
2023-12-26 02:33:27 +0100

    feat: migrate from jest to vitest for unit test

--- libs/backend/shared/vite.config.ts
+++ libs/backend/shared/vite.config.ts
@@ -1,10 +1,17 @@
-/// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import * as path from 'path';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+  test: {
+    globals: true,
+    cache: {
+      dir: '../../../node_modules/.vitest',
+    },
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
   cacheDir: '../../../node_modules/.vite/backend-shared',
 
   plugins: [
--- libs/frontend/ui/shared/vite.config.ts
+++ libs/frontend/ui/shared/vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
@@ -6,6 +5,14 @@ import * as path from 'path';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+  test: {
+    globals: true,
+    cache: {
+      dir: '../../../../node_modules/.vitest',
+    },
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
   cacheDir: '../../../../node_modules/.vite/frontend-ui-shared',
 
   plugins: [
